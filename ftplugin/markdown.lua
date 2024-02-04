35b924fb5dd13db52cedfd3048b6f0b9cea0cc5a
Daniel Palamarchuk <danielpalamarchuk@gmail.com>
2024-02-04 17:56:49 -0500

    Added some vimwiki functionality into vimwiki-less config

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -1,2 +1,33 @@
-
 vim.keymap.set("n", "<leader>wh", ":ObsidianTemplate default.md<CR>")
+vim.keymap.set("i", "<CR>", function ()
+    vim.cmd("normal! ^")
+    local cword = vim.fn.expand('<cWORD>')
+    if cword == "*" or cword == "-" then
+        vim.cmd("normal! o* ")
+    else
+        vim.cmd("normal! o")
+    end
+    vim.cmd('startinsert!')
+end)
+
+vim.keymap.set("n", "o", function ()
+    vim.cmd("normal! ^")
+    local cword = vim.fn.expand('<cWORD>')
+    if cword == "*" or cword == "-" then
+        vim.cmd("normal! o* ")
+    else
+        vim.cmd("normal! o")
+    end
+    vim.cmd('startinsert!')
+end)
+
+vim.keymap.set("n", "O", function ()
+    vim.cmd("normal! ^")
+    local cword = vim.fn.expand('<cWORD>')
+    if cword == "*" or cword == "-" then
+        vim.cmd("normal! O* ")
+    else
+        vim.cmd("normal! O")
+    end
+    vim.cmd('startinsert!')
+end)
